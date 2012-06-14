3cb492d5657b20ce1d5499b8a04612f424b8c514
Jason <jason@ezimerchant.com>
2012-06-14 23:34:46 +1000

    first commit

---
+++ clean.sh
@@ -0,0 +1,2 @@
+rm -f public_html/mobile.php public_html/ezifind.php public_html/.htaccess public_html/catalog/checkout_calculate.php public_html/catalog/checkout_complete.php
+rm -rf public_html/ezimobile 
